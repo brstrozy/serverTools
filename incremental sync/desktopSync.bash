197dcec5ee06dd92b5ed979a11808076bc08cf6a
Benjamin <brstrozy@buffalo.edu>
2023-01-27 18:06:05 -0500

    updated desktop/incrementalsync

--- incremental sync/desktopSync.bash
+++ incremental sync/desktopSync.bash
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-inputFile="/mnt/user/storage/serverConfig/desktop.txt"
-HOST="192.168.1.80"
-LOG_DIR="/mnt/user/logs/desktopSync/"`date +"%m-%d-%Y"`""
-TRASH_PATH="/mnt/user/trash/"
-BACKUP_PATH="/mnt/user/desktop/"
+# CRON SETUP
+#run every 5 minutes, flock used to make sure another instance of rsync/script doesn't start while the previous is still running
+#
+# m h dom mon dow user  command
+# 5 * * * * root /usr/bin/flock -w 0 /var/cron.lock /usr/bin/myscript
+
+#--------------------------------------------------------------------------------------------
+# VARIABLES
+#--------------------------------------------------------------------------------------------
+
+# INPUT FILE FORMAT: input path alone (no extra space, commas, etc.) or inputpath,outputpath if you need to change the output for specific input paths
+INPUT_FILE="PATH"
+LOG_DIR="PATH/"`date +"%m-%d-%Y"`""
+TRASH_PATH="PATH"
+BACKUP_PATH="PATH"
 
 LOG_FILEPATH="$LOG_DIR"/desktop-backup.txt""
 HAD_ERROR=0
 
+#--------------------------------------------------------------------------------------------
+# INIT
+#--------------------------------------------------------------------------------------------
+
 #Make log directory if not exists
 mkdir -p "$LOG_DIR"
 
-# INPUT FILE FORMAT: input path alone (no extra space, commas, etc.) or inputpath,outputpath if you need to change the output for specific input paths
-file=$(cat $inputFile)
-
 # GET ARRAY OF PATHS FROM file
-readarray -t paths < $inputFile
-
-# echo -e "ARRAY: \n"
-# for (( i=0; i<=${#paths[@]}; i++ )); do
-#     echo "${paths[$i]}" >> "$LOG_FILEPATH"
-# done
+readarray -t paths < $INPUT_FILE
 
 #--------------------------------------------------------------------------------------------
+# BACKUP
 #--------------------------------------------------------------------------------------------
 
 echo -e "Starting Backup..." >> "$LOG_FILEPATH"
 
-ping -q -c 1 $HOST > /dev/null
-SKIP=$?
-
-# ERROR HANDLING IF HOST PC OFFLINE
-if [ $SKIP -ne 0 ]; then
-    TIME="`date + "%T"`"
-    echo -e "[ $TIME ] Error: ping exited with error code $SKIP on host $HOST" >> "$LOG_FILEPATH"
-    echo -e "[ $TIME ] Host likely offline" >> "$LOG_FILEPATH"
-fi
-
-# BACK UP EACH DIRECTORY IN inputFile
+# BACK UP EACH DIRECTORY IN INPUT_FILE
 if [ $SKIP -eq 0 ]; then
     for (( i=0; i<=${#paths[@]}; i++ )); do
     
@@ -60,7 +57,7 @@ if [ $SKIP -eq 0 ]; then
         fi
 
         # Sync files between two locations, move differing/deleted items to trash directory
-        rsync -xavh --progress --delete --backup --backup-dir="$TRASH_PATH" "$SRC" "$DEST" &>> "$LOG_FILEPATH"
+        rsync -xavhi --delete --backup --backup-dir="$TRASH_PATH" "$SRC" "$DEST" &>> "$LOG_FILEPATH"
 
         # Send an error email if rsync exits with non 0 exit code.
         if [ $? -ne 0 ]; then
@@ -77,7 +74,9 @@ echo -e "\n\n\n\n\n" >> "$LOG_FILEPATH"
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 
-#Send Completion Email if no errors occurred
-if [ $HAD_ERROR -eq 0 ]; then
-	/usr/local/emhttp/webGui/scripts/notify -e "Desktop Sync Status" -i normal -s "Desktop Sync Complete!"
-fi
+
+# DISABLED EMAIL SUCCESS DUE TO REPETITIVE NATURE OF INCREMENTAL SYNCING
+# #Send Completion Email if no errors occurred
+# if [ $HAD_ERROR -eq 0 ]; then
+# 	/usr/local/emhttp/webGui/scripts/notify -e "Desktop Sync Status" -i normal -s "Desktop Sync Complete!"
+# fi
